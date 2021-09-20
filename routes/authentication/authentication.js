4e8f9e561bf6aa6a415f08b4ee0ac07d26ad188d
Viren Patil <virenpatil1@outlook.com>
2021-09-20 14:59:46 +0200

    Created the middleware to check the type of user on login. Created the bcrypt password to check the existing password for login

--- routes/authentication/authentication.js
+++ routes/authentication/authentication.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { check, validationResult } from 'express-validator';
 import { IsHR, IsUser } from '../../middlewares/checkUserExist.js';
 import Candidate from '../../models/Candidate.js';
+import { WhichUser } from '../../middlewares/typeOfUser.js';
+import Hr from '../../models/Hr.js';
 const router = express.Router();
 
 // get looged in user
@@ -14,6 +17,7 @@ router.get('/user', (req, res) => {
 router.post(
   '/login',
   // middleware for checking the response body by express-validator
+  WhichUser,
   [
     check('email', 'Please Enter the Email').isEmail(),
     check(
@@ -21,12 +25,68 @@ router.post(
       'Please enter the password more than 6 characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    let { email, password, typeOfUser } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('pass');
+    // Compare the password of the typeof user in the particular database
+    try {
+      if (typeOfUser === 'Candidate') {
+        let user = await Candidate.findOne({ email: email });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+          const payload = {
+            candidate: {
+              id: user.id,
+            },
+          };
+          jwt.sign(
+            payload,
+            process.env.JWTSECRET,
+            {
+              expiresIn: 36000,
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+        } else {
+          res.send('Password Incorrect');
+        }
+      } else if (typeOfUser === 'Hr') {
+        let user = await Hr.findOne({ email: email });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+          const payload = {
+            hr: {
+              id: user.id,
+            },
+          };
+          jwt.sign(
+            payload,
+            process.env.JWTSECRET,
+            {
+              expiresIn: 36000,
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+        } else {
+          res.send('Password Incorrect');
+        }
+      } else {
+        res.send('No User not found');
+      }
+    } catch (error) {
+      console.log(error.message);
+
+      res.status(500).send('Server Error');
+    }
   }
 );
 
@@ -63,7 +123,24 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       candidate.password = await bcrypt.hash(password, salt);
       await candidate.save();
-      res.send('Candidate Saved');
+
+      // Creating and Sending the Jwt token for verification
+      const payload = {
+        candidate: {
+          id: candidate.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        process.env.JWTSECRET,
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.log(error.message);
       res.status(500).send('Server Error');
